beb05e5ba67aec6232f61df6bdc25d1dbe1f1220
D1scoDancer <apshulikov@edu.hse.ru>
2023-02-04 13:55:05 +0300

    send test data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import React, { useState } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
 
 import Menu from "./components/Menu/Menu"
 import Test from "./components/Test/Test"
@@ -14,6 +15,42 @@ function App() {
 
     const btn0OnClickHandler = () => {
         setTestVisible(!testVisible)
+    }
+
+    const sendAnswers = () => {
+        fetch("https://be95-83-242-179-144.eu.ngrok.io/answer/save-answer", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                answer1: "TEST",
+                answer2: "TEST",
+                answer3: "TEST",
+                answer4: "TEST",
+                answer5: "TEST",
+                answer6: "TEST",
+                answer7: "TEST",
+                answer8: "TEST",
+                answer9: "TEST",
+                answer10: "TEST",
+                answer11: "TEST",
+                answer12: "TEST",
+                answer13: "TEST",
+                answer14: "TEST",
+                answer15: "TEST",
+                answer16: "TEST",
+                answer17: "TEST",
+                answer18: "TEST",
+                answer19: "TEST",
+                answer20: "TEST",
+                answer21: "TEST",
+                answer22: "TEST",
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.error(error))
     }
 
     return (
@@ -37,7 +74,7 @@ function App() {
                             <Row className="mt-5">
                                 <Test quizData={quizData} />
                             </Row>
-                            <Button>Submit</Button>
+                            <Button onClick={sendAnswers}>Submit</Button>
                         </div>
                     )}
                 </Container>
