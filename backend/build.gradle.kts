1726a9668a93426d60c6719a57bf57aecb013eee
unknown <jush1703@gmail.com>
2023-02-13 02:29:14 +0300

    Deployment configured

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.postgresql:postgresql")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
